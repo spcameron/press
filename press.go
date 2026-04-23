b7b7cafaf950047174af85d8030048571418495e
spcameron <cameron.seanpatrick@gmail.com>
2026-04-23 09:08:52 -0400

    Add home page building
    Implements a slice capable of building a single home page.
    
    Build() accepts the output dir via BuildOptions and Renderers struct,
    creates the output directory and calls to buildHome().
    
    buildHome() receives the out dir, the site data (currently an empty
    struct), and the Renderers. Nil guard check on the required render func,
    followed by defining the filepath (out + index.html), then calls to
    writeRendered() while passing the path and a func wrapper that returns
    the render func, now bearing the site data.
    
    writeRendered() ensures the directory exists (defensive), creates the
    file at the provided filepath, calls the render callback func while
    providing the new file as the io.Writer argument, and then closes the
    file.

--- press.go
+++ press.go
@@ -1,6 +1,11 @@
 package press
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
 
 type BuildOptions struct {
 	OutDir string
@@ -16,6 +21,63 @@ type Renderers struct {
 	Home func(w io.Writer, data HomePageData) error
 }
 
-func Build(opts BuildOptions) ([]string, error) {
-	return []string{}, nil
+func Build(opts BuildOptions, r Renderers) ([]string, error) {
+	out := filepath.Clean(opts.OutDir)
+
+	if err := os.MkdirAll(out, 0o755); err != nil {
+		return nil, fmt.Errorf("mkdir: %s: %w", out, err)
+	}
+
+	site := SiteData{}
+
+	var written []string
+
+	if w, err := buildHome(out, site, r); err != nil {
+		return nil, err
+	} else {
+		written = append(written, w...)
+	}
+
+	return written, nil
+}
+
+func buildHome(out string, site SiteData, r Renderers) ([]string, error) {
+	if r.Home == nil {
+		return nil, fmt.Errorf("missing Home renderer")
+	}
+
+	path := filepath.Join(out, "index.html")
+	data := HomePageData{
+		Site: site,
+	}
+
+	if err := writeRendered(path, func(w io.Writer) error {
+		return r.Home(w, data)
+	}); err != nil {
+		return nil, err
+	}
+
+	return []string{path}, nil
+}
+
+func writeRendered(path string, render func(io.Writer) error) error {
+	if err := os.MkdirAll(filepath.Dir(path), 0o755); err != nil {
+		return fmt.Errorf("mkdir: %s: %w", filepath.Dir(path), err)
+	}
+
+	f, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("create: %s: %w", path, err)
+	}
+
+	if err := render(f); err != nil {
+		_ = f.Close()
+		return fmt.Errorf("render: %s: %w", path, err)
+	}
+
+	if err := f.Close(); err != nil {
+		return fmt.Errorf("close: %s: %w", path, err)
+	}
+
+	return nil
 }
